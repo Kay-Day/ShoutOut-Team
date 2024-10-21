d5df8edf818dcfb86a8c16a9a705213e0bd70aba
Le Nhu Nghia Nguyen (グエン・レ・ニュ・ギア) <92531311+Kay-Day@users.noreply.github.com>
2024-10-21 17:55:37 +0700

    San pham

--- lib/views/screens/category.Screen.dart
+++ lib/views/screens/category.Screen.dart
@@ -1,5 +1,6 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -9,10 +10,86 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final Stream<QuerySnapshot> _categoriesStream =
+      FirebaseFirestore.instance.collection('categories').snapshots();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Category Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Icon(
+              Icons.category,
+              color: Colors.black,
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Sản phẩm',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _categoriesStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: GridView.builder(
+              itemCount: snapshot.data!.docs.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, mainAxisSpacing: 8, crossAxisSpacing: 8),
+                itemBuilder: (context, index) {
+                  final categoryData = snapshot.data!.docs[index];
+                  return Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            blurRadius: 4,
+                            offset: Offset(0, 2),
+                          )
+                        ],),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.network(categoryData['image'],
+                            width: 80,
+                            height: 80,
+                            ),
+                            SizedBox(height: 10,),
+                            Text(categoryData['categoryName'].toUpperCase(),
+                            style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,),
+                            )
+
+                          ],
+                        ),
+                  );
+                }),
+          );
+        },
+      ),
     );
   }
 }
